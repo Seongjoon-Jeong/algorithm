47655a2c8fa82baa995e2721418047f38c2fac84
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-02-02 21:44:45 +0900

    [level 0] Title: 수 조작하기 2, Time: 11.19 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,6 +1,5 @@
 const solution = (numLog) => {
     let answer = "";
-    const maxLength = 100000;
     
     numLog.forEach((value, idx) => {
         const diff = (idx === numLog.length) ? "" : numLog[idx + 1] - value;
