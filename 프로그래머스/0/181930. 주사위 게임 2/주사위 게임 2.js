4692538647c7654987945157986345605587fbf2
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-02-02 19:20:33 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+const solution = (a, b, c) => {
+    let answer = 0;
+    
+    if (a === b && b === c) {
+        answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+    } else if (a !== b && a !== c && b !== c) {
+        answer = a + b + c;
+    } else {
+        answer = (a + b + c) * (a*a + b*b + c*c);
+    }
+    
+    return answer;
+}
