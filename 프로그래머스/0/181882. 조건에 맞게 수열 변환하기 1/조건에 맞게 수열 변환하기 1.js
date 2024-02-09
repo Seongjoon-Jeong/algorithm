5a7518ed738ddb4089ec9ce67215c6530fc6d5e2
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-02-10 00:43:39 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 18.01 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+const solution = (arr) => {
+    return arr.map((val) => {
+        if (val >= 50 && val % 2 === 0) {
+            return val / 2;
+        } else if (val < 50 && val % 2 !== 0) {
+            return val * 2;
+        } else {
+            return val;
+        }
+    });
+}
