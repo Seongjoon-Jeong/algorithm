0b14ba9a0b820e2c7a9182e618a89e26f1e71ae9
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-02-16 00:54:28 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 11.39 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -0,0 +1,47 @@
+const solution = (n) => {
+    let arr = new Array(n).fill(0).map((v) => {
+        return [...new Array(n).fill(1)];
+    });
+    let i = 0, j = 0, mode = 0;
+    
+    arr[0][0] = 1;
+    for (let k = 2; k <= n * n; k++) {
+        switch (mode % 4) {
+            case 0: // ->
+                j++;
+                arr[i][j] = k;
+                console.log('->', k); //
+                if (j === n - 1 - Math.floor(mode / 4)) {
+                    mode++;
+                }
+                break;
+            case 1: // v
+                i++;
+                arr[i][j] = k;
+                console.log('v', k, i, j); //
+                if (i === n - 1 - Math.floor(mode / 4)) {
+                    mode++;
+                }
+                break;
+            case 2: // <-
+                j--;
+                arr[i][j] = k;
+                console.log('<-', k); //
+                if (j === Math.floor(mode / 4)) {
+                    mode++;
+                }
+                break;
+            case 3: // ^
+                i--;
+                arr[i][j] = k;
+                console.log('^', k); //
+                if (i === Math.floor(mode / 4) + 1) {
+                    mode++;
+                }
+                break;
+        }
+    }
+    
+    console.log(mode, Math.floor(mode / 4) + 1); //
+    return arr;
+}
