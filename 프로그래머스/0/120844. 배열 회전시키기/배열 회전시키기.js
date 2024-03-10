3564d0a94adeeb92c23e0b5cbb8a21b615dc54bc
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-03-10 22:39:25 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,3 @@
+const solution = (numbers, direction) => {
+    return direction === "left" ? [...numbers.slice(1), numbers[0]] : [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)];
+}
