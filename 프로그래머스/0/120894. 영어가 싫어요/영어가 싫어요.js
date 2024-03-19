d8f6b7dda8424d1c3711ed8d385b6c15bb88fac0
Seongjoon <158159906+Seongjoon-Jeong@users.noreply.github.com>
2024-03-19 15:44:17 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,6 +1,5 @@
 const solution = (numbers) => {
     const numAlphabet = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
-    const numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let answer = '';
     
     numbers = [...numbers];
@@ -8,7 +7,7 @@ const solution = (numbers) => {
     for (let i = 0; i < numbers.length;) {
         for (let j = 0; j < numAlphabet.length; j++) {
             if (numAlphabet[j].slice(0, 3) === numbers.slice(i, i + 3).join('')) {
-                answer += numList[j];
+                answer += j;
                 i += numAlphabet[j].length;
                 break;
             }
